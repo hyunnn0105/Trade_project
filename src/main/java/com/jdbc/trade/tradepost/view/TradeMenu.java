57f166b59186b7d2ce829965a9e501cc62d86afa
hyunnn0105 <asha010519@gmail.com>
2022-07-03 23:29:44 +0900

    220703 home-수정

--- src/main/java/com/jdbc/trade/tradepost/view/TradeMenu.java
+++ src/main/java/com/jdbc/trade/tradepost/view/TradeMenu.java
@@ -3,6 +3,7 @@ package com.jdbc.trade.tradepost.view;
 import com.jdbc.trade.tradepost.controller.TradeController;
 import com.jdbc.trade.tradepost.domain.Trade;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class TradeMenu {
@@ -33,16 +34,16 @@ public class TradeMenu {
                     insetinformation();
                     break;
                 case 2:
-//                    findsomeinfor();
+                    findsomeinfor();
                     break;
                 case 3:
-//                    showAllinfor();
+                    showAllinfor();
                     break;
                 case 4:
-//                    updatePostNum();
+                    updatePostNum();
                     break;
                 case 5:
-//                    deleteinfor();
+                    deleteinfor();
                 case 9:
                     System.out.println("시스템 종료");
                     System.exit(0);
@@ -55,22 +56,98 @@ public class TradeMenu {
         }
     }
 
+    private void deleteinfor() {
+        System.out.println(" 삭제 할 번호를 입력하세요");
+        int num = inputN(">> ");
+
+        if (tradeController.findSomeinfor(num)!=null){
+            boolean flag = tradeController.deleteinfor(num);
+            if (flag){
+                System.out.println(" 삭제가 완료되었습니다.");
+            }else {
+                System.out.println(" 삭제에 실패했습니다.");
+            }
+        } else {
+            System.out.println(" 해당 번호는 존재하지 않는 번호입니다.");
+        }
+    }
+
+    private void updatePostNum() {
+        System.out.println("========= 운송장번호 등록하기 ===========");
+        int num = inputN(">> ");
+
+        if (tradeController.findSomeinfor(num) != null){
+            System.out.println("등록하실 운송장번호를 입력해주세요");
+            String postNum = inputS(" - 운송장번호 : ");
+
+            boolean flag = tradeController.updatePostinfor(postNum, num);
+            if (flag){
+                System.out.println("수정이 완료되었습니다.");
+            } else {
+                System.out.println("수정이 실패했습니다.");
+            }
+
+        } else {
+            System.out.println("해당 번호는 존재하지 않는 번호입니다.");
+        }
+    }
+
+    private void showAllinfor() {
+
+        List<Trade> trades = tradeController.findAllinformation();
+
+        System.out.println("========= 모든 회원 정보 ============\n");
+
+        System.out.printf("%s%d%d%s%d%d%d%d\n", "번호", "이름", "종류", "가격", "휴대폰 번호", "배송방법", "주소", "운송장번호");
+        System.out.println("-------------------------------------------- 조회결과 ----------------------------------------");
+        for (Trade t : trades) {
+            System.out.println(t.getTraNu() + t.getTraNm() + t.getItem() + t.getPrice() + t.getTraPhone() + t.getPostMethod()
+            + t.getPostAddr() + t.getPostNum());
+        }
+
+
+    }
+
+    private void findsomeinfor() {
+        System.out.println("조회할 회원의 번호를 입력하세요.");
+        int num = inputN(">> ");
+
+        Trade trade = tradeController.findSomeinfor(num);
+        if (trade != null){
+            System.out.println("\n조회결과");
+            System.out.println(" - 번호 : " + trade.getTraNu());
+            System.out.println(" - 이름 : " + trade.getTraNm());
+            System.out.println(" - 종류 : " + trade.getItem());
+            System.out.println(" - 가격 : " + trade.getPrice());
+            System.out.println(" - 휴대폰 번호 : " + trade.getTraPhone());
+            System.out.println(" - 배송방법: " + trade.getPostMethod());
+            System.out.println(" - 주소: " + trade.getPostAddr());
+            System.out.println(" - 운송장번호: " + trade.getPostNum());
+
+        }else {
+            System.out.println("\n 해당 회원은 존재하지 않습니다.");
+        }
+    }
+
     private void insetinformation() {
+
+
         System.out.println("================================");
         System.out.println("배송정보 추가");
         System.out.println();
         int number = inputN("번호 : ");
+        if (tradeController.findSomeinfor(number)!=null){
+            System.out.println("이미 존재하는 번호입니다. 다시 입력해주세요");
+            return;
+        }
         String name = inputS("이름 : ");
         String item = inputS("종류 : ");
         int price = inputN("가격 : ");
         String phone = inputS("휴대폰 번호('-'없이 입력하세요) : ");
         String postMethod = inputS("배송방법 : ");
         String postAddr = inputS("주소 : ");
-        String postNum = inputS("운송장번호: ");
+        String postNum = inputS("운송장번호 : ");
 
-        if (tradeController.findnum(number) == number){
-            System.out.println("number의 숫자를 다시 입력하세요.");
-        }
 
         Trade trade = new Trade();
         trade.setTraNu(number);
